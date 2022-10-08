ea911d3339803ba74011c15d6179187d8695b451
Surbhi Nimje <surbhi.nimje@gmail.com>
2022-10-09 02:11:06 +0530

    tic-tac-toe game

--- src/index.js
+++ src/index.js
@@ -1,17 +1,82 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React,{useState} from 'react';
+import ReactDOM  from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const Square=(props)=>{
+  
+  return(
+    <button  className="square" onClick={props.onClickEvent}>
+      {props.value}
+    </button>
+  );
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Board=()=>{
+  const initialSquares=Array(9).fill(null);
+  const [squares,setSquares]=useState(initialSquares);
+  const [xIsNext,setXIsNext]=useState(true);
+  const handleClickEvent=(i)=>{
+    const newSquares=[...squares];
+    const winnerDeclared=Boolean(calculateWinner(newSquares));
+    const sqaureFilled=Boolean(newSquares[i]);
+    if(winnerDeclared||sqaureFilled){
+      return;
+    }
+    newSquares[i]=xIsNext?'X':'O';
+    setSquares(newSquares);
+    setXIsNext(!xIsNext);
+  };
+    const renderSquare=(i)=>{
+    return (<Square 
+    value={squares[i]} 
+    onClickEvent={()=>handleClickEvent(i)}
+    />
+    );
+  };
+  const winner=calculateWinner(squares);
+  const status=winner?`Winner: ${winner}`:`Next player: ${xIsNext?'X':'O'}`;
+  return(
+    <div>
+    <div className='status'>{status}</div>
+  <div className='board-row'>
+    {renderSquare(0)}{renderSquare(1)}{renderSquare(2)}
+  </div>
+  <div className='board-row'>
+  {renderSquare(3)}{renderSquare(4)}{renderSquare(5)}
+  </div>
+  <div className='board-row'>
+  {renderSquare(6)}{renderSquare(7)}{renderSquare(8)}
+  </div>
+  </div>
+  );
+};
+
+const Game=()=>{
+  return (
+    <div className='game'>
+      Tic-Tac-Toe
+      <Board/>
+    </div>
+  );
+};
+
+ReactDOM.render(<Game/>,document.getElementById('root'));
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
